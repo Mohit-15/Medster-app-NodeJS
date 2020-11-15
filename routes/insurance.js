a10fe6da08f52c17e2986482ab439c810bedf50f
Mohit-15 <programs.mohit15@gmail.com>
2020-11-16 02:20:49 +0530

    update routes

--- routes/insurance.js
+++ routes/insurance.js
@@ -2,13 +2,7 @@ require('dotenv').config()
 var express = require("express"),
   mongoose = require("mongoose"),
   registration_router = express.Router(),
-  path = require('path'),
-  User = require('../models/user'),
-  Token = require('../models/token'),
-  Insurance = require('../models/insurance'),
-  crypto = require('crypto'),
-  mailer = require('nodemailer'),
-  bodyParser = require("body-parser");
+  Insurance = require('../models/insurance');
 
 registration_router.get('/users/add-insurance/admin/123456', function(req, res){
   res.render('add_insurance')
